f66d6ae22dd07cd5e80b5c46352d76638cecb85e
Thomas Graeff <676@holbertonschool.com>
2019-06-24 21:37:10 +0000

    Adds task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * main - creates 5 zombie processes
+ * Return: 0
+ */
+int main(void)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		printf("Zombie process created, PID: %d\n", getpid());
+		exit(0);
+	}
+
+	infinite_while();
+	return (0);
+}
+
+/**
+ * infinite_while - sleep 1 between processes
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
